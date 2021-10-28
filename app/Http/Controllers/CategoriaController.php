98008e24585973cff9124bd145bd7254d6856e4b
unknown <hamerpantoja@gmail.com>
2021-10-28 16:45:08 -0500

    Metodo Listar Datos

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
+
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
     //
+  public function index() {
+      $categoria=Categoria::select('id','nombre')->orderBy('nombre','desc')->get();
+      return [
+          'cat'=>$categoria
+      ];
+  }
+
+
+   public function store(Request $request) {
+   $categoria= new Categoria();
+   $categoria->nombre=$request->nombre;
+   $categoria->edo=$request->edo;
+
+   $categoria->save();
+
+   }
+   
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\MarcaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/categoria', [CategoriaController::class, 'index']);
+Route::post('/api/categoria/registrar', [CategoriaController::class, 'store']);
+Route::post('/api/cliente/registrar', [ClienteController::class, 'store']);
+Route::post('/api/marca/registrar', [MarcaController::class, 'store']);
